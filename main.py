24f203682719aaf7b8e1f0be86b014d69efc3830
dotrdp <rckydp1@gmail.com>
2026-04-25 19:03:47 -0600

    added drawing app from another repo for modeling maps, which will then be turned into graphs

---
+++ main.py
@@ -0,0 +1,15 @@
+
+
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer, AutoProcessor
+from trl.trainer.grpo_trainer import GRPOTrainer, GRPOConfig
+
+import networkx as nx
+
+
+def main():
+    print("Hello from rl-2agents-chase!")
+
+
+if __name__ == "__main__":
+    main()
